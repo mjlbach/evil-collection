307652b0947013f86470fbb22ef95ab0f949b2e5
Michael Lingelbach <m.j.lbach@gmail.com>
2021-01-27 19:52:27 -0800

    Test

--- modes/vterm/evil-collection-vterm.el
+++ modes/vterm/evil-collection-vterm.el
@@ -87,7 +87,7 @@ also uses `evil-mode'."
   (interactive)
   (let ((inhibit-read-only t))
     (cl-letf (((symbol-function 'delete-region) 'vterm-delete-region))
-      (call-interactively 'evil-delete))))
+      (call-interactively #'evil-delete))))
 
 (defun evil-collection-vterm-delete-line ()
   "Provide similar behavior as `evil-delete-line'."
